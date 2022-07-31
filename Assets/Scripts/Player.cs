a8888597536d0a5970a2dae70196a66aa62b0e02
EE539 <56602339+EE539@users.noreply.github.com>
2022-07-31 22:22:53 +0300

    Basic Demo finished

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,14 +6,14 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     Rigidbody rb;
-
+    public ParticleSystem particle;
     public InputActionAsset map;
     InputActionMap gameplay;
     InputAction movement, startMovement;
 
     public float movementSpeed = 5f, rotateSpeed = 5f;
     float rotationValue = 0f;
-    bool isMoving = false;
+    bool isMoving = false, startFinishSequence = false, gameFinished = false;
 
     private void Awake()
     {
@@ -49,26 +49,44 @@ public class Player : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (isMoving)
+        if (!startFinishSequence)
         {
-            //rb.AddForce(transform.forward * movementSpeed); //Move player
-            transform.Rotate(Vector3.up * rotationValue);
-            transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
-            if (this.transform.rotation.y > 0.90f)
-                transform.rotation = Quaternion.Euler(0, 90f, 0);
-            else if (this.transform.rotation.y < -0.90f)
-                transform.rotation = Quaternion.Euler(0, -90f, 0);
-        }
-        else
-        {
+            if (isMoving)
+            {
+                //rb.AddForce(transform.forward * movementSpeed); //Move player
+                transform.Rotate(Vector3.up * rotationValue);
+                transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+                if (this.transform.rotation.y > 0.90f)
+                    transform.rotation = Quaternion.Euler(0, 90f, 0);
+                else if (this.transform.rotation.y < -0.90f)
+                    transform.rotation = Quaternion.Euler(0, -90f, 0);
+            }
             rotationValue = 0f;
             transform.Rotate(Vector3.up * rotationValue);
         }
+        else
+        {
+            while (transform.position.y < 10 && transform.position.z < 96)
+            {
+                if(transform.rotation.x < 0.45f)
+                    transform.Rotate(Vector3.right * Time.deltaTime);
+                transform.Translate(new Vector3(0, 1, 1) * Time.deltaTime * movementSpeed / 10);
+                if (transform.position.y > 10)
+                   transform.position = new Vector3(transform.position.x, 10, transform.position.z);
+                if (transform.position.z > 96)
+                   transform.position = new Vector3(transform.position.x, transform.position.y, 96);
+            }
+
+                particle.Play();
+                
+            
+                
+        }
+        
         
     }
-
 
     private void OnEnable()
     {
@@ -77,5 +95,14 @@ public class Player : MonoBehaviour
     private void OnDisable()
     {
         gameplay.Disable();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Finish")
+        {
+            startFinishSequence = true;
+           gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        }
     }
 }
