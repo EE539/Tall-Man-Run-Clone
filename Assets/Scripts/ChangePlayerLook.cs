a1b8a805ad06dcff4d4d9fa1c85667f07ded1b86
EE539 <56602339+EE539@users.noreply.github.com>
2022-07-31 16:47:20 +0300

    Hitting lowe obstacle is finished

--- Assets/Scripts/ChangePlayerLook.cs
+++ Assets/Scripts/ChangePlayerLook.cs
@@ -5,15 +5,18 @@ using UnityEngine.UI;
 
 public class ChangePlayerLook : MonoBehaviour
 {
+    Color originalColor;
     public Text numberText;
+    float timer = 0, willDie = 1;
     int number;
-    int willDie = 0;
+    bool destroyObject = true, repeat = true;
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             float xScale = collision.gameObject.transform.localScale.x, yScale = collision.gameObject.transform.localScale.y, zScale = collision.gameObject.transform.localScale.z;
-            number = int.Parse(numberText.text);
+            if(numberText != null)
+                number = int.Parse(numberText.text);
             if(gameObject.tag == "Thicker")
             {
                 xScale += (float) number / 1000;
@@ -37,15 +40,36 @@ public class ChangePlayerLook : MonoBehaviour
                 yScale += (float)number / 1000;
                 willDie--;
             }
-            if (willDie >= 0)
+            else if(gameObject.tag == "Low Obstacle")
+            {
+                destroyObject = false;
+                originalColor = collision.gameObject.GetComponent<MeshRenderer>().material.color;
+                willDie -= 0.1f;
+                xScale -= 0.01f;
+                yScale -= 0.01f;
+                zScale -= 0.01f;
+                StartCoroutine(ChangeColor(collision.gameObject, originalColor));
+                collision.gameObject.transform.Translate(Vector3.forward * (-1));
+            }
+            if (willDie > 0)
                 collision.gameObject.transform.localScale = new Vector3(xScale, yScale, zScale);
             else
             {
                 collision.gameObject.transform.localScale = new Vector3(0.5f, 0.246f, 0.5f);
                 Debug.Log("Game Over ");
             }
-
-            Destroy(gameObject);
+            if(destroyObject)
+                Destroy(gameObject);
         }
+    }
+
+    IEnumerator ChangeColor(GameObject player, Color color)
+    {
+        Debug.Log("entered");
+        player.GetComponent<MeshRenderer>().material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        player.GetComponent<MeshRenderer>().material.color = color;
+        gameObject.SetActive(false);
+        gameObject.SetActive(true);
     }
 }
